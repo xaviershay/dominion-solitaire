474801d0656925c45daad55c8aa7f0ab9045aef7
Xavier Shay <xavier@rhnh.net>
2010-05-30 21:25:05 +1000

    Start merging the two tests

--- lib/data-test.rb
+++ lib/data-test.rb
@@ -1,7 +1,10 @@
+require 'ffi-ncurses'
+
 module Dominion
   module Util
     def generate_names!(cards)
       cards.each do |key, values|
+        values[:key]  = key
         values[:name] = key.to_s
       end
     end
@@ -42,7 +45,7 @@ module Dominion
       player[:hand].delete_at(player[:hand].index(card))
       player[:played] << card
 
-      card[:behaviour][player]
+      card[:behaviour][player, card]
     end
   end
 
@@ -70,18 +73,29 @@ class Game
       :duchy => {
         :type => :victory,
         :cost => 5},
+      :provence => {
+        :type => :victory,
+        :cost => 8},
       :copper => {
         :type => :treasure,
         :cost => 0},
       :silver => {
         :type => :treasure,
         :cost => 3},
+      :gold => {
+        :type => :treasure,
+        :cost => 6},
+      :curse => {
+        :type => :victory,
+        :cost => 0},
         
       :cellar => {
         :type => :action,
         :cost => 2,
-        :behaviour => lambda {|player|
-          player[:actions] += 1
+        :actions => 1,
+        :description => 'Discard X cards, draw X cards',
+        :behaviour => lambda {|player, card|
+          player[:actions] += card[:actions]
 
           discard_count = 0
           while discarded = prompt_player_for_card_in_hand(player, :prompt => "Choose a card to discard", :required => false) 
@@ -96,8 +110,12 @@ class Game
     @board = [
       [cards[:copper]] * 60,
       [cards[:silver]] * 40,
+      [cards[:gold]] * 30,
       [cards[:estate]] * 8,
-      [cards[:duchy]]  * 8
+      [cards[:duchy]]  * 8,
+      [cards[:provence]]  * 8,
+      [cards[:curse]] * 30,
+      [cards[:cellar]]  * 8
     ]
 
     @player = {
@@ -125,6 +143,128 @@ class Game
     play_card(player, :cellar)
     pp player
   end
+  
+  def run
+    painter = Painter.new
+    begin
+      ctx = painter.draw(self)
+      painter.wait_for_input(ctx)
+    ensure
+      painter.finalize
+    end
+  end
+
+  class Painter
+    include FFI::NCurses
+
+    def initialize
+      initscr
+      start_color
+  
+      # set up colour pairs
+      #             Background       Foreground
+      init_pair(0,  Colour::BLACK,   Colour::BLACK)
+      init_pair(1,  Colour::RED,     Colour::BLACK)
+      init_pair(2,  Colour::GREEN,   Colour::BLACK)
+      init_pair(3,  Colour::YELLOW,  Colour::BLACK)
+      init_pair(4,  Colour::BLUE,    Colour::BLACK)
+      init_pair(5,  Colour::MAGENTA, Colour::BLACK)
+      init_pair(6,  Colour::CYAN,    Colour::BLACK)
+      init_pair(7,  Colour::WHITE,   Colour::BLACK)
+
+      init_pair(8,  Colour::BLACK,   Colour::BLACK)
+      init_pair(9,  Colour::BLACK,   Colour::RED)
+      init_pair(10, Colour::BLACK,   Colour::GREEN)
+      init_pair(11, Colour::BLACK,   Colour::YELLOW)
+      init_pair(12, Colour::BLACK,   Colour::BLUE)
+      init_pair(13, Colour::BLACK,   Colour::MAGENTA)
+      init_pair(14, Colour::BLACK,   Colour::CYAN)
+      init_pair(15, Colour::BLACK,   Colour::WHITE)
+    end
+
+    def wait_for_input(ctx)
+      wgetch(ctx)
+    end
+
+    def finalize
+      endwin
+    end
+
+    def draw(game)
+      curs_set 0
+     
+      board_frame = newwin(14, 80, 0, 0)
+      board = newwin(12, 78, 1, 1)
+
+      print_with_color = lambda do |color, text|
+        color_index = {
+          :white        => 7,
+          :yellow       => 3,
+          :blue         => 4,
+          :red          => 1,
+          :cyan_back    => 14,
+          :green_back   => 10,
+          :magenta_back => 13,
+          :yellow_back  => 11
+        }[color] || raise("Unknown color: #{color}")
+        wattr_set board, A_NORMAL, color_index, nil
+
+        wprintw(board, text.to_s)
+      end
+
+      type_char = {
+        :treasure => 'T',
+        :action   => 'A',
+        :victory  => 'V'
+      }
+
+      max_name_length = game.board.map {|pile| pile[0][:name].length }.max
+      
+      header, body = game.board.partition {|x|
+        [
+          :copper,
+          :silver,
+          :gold,
+          :estate,
+          :duchy,
+          :provence,
+          :curse
+        ].include?(x.first[:key])
+      }
+
+      header.each do |pile|
+        card = pile.first
+
+        print_with_color[:yellow, card[:cost]]
+        print_with_color[:red,    type_char[card[:type]]]
+        print_with_color[:blue,   pile.size]
+        print_with_color[:white,  " %s" % card[:name]]
+        print_with_color[:white, ' ']
+      end
+
+      print_with_color[:white, "\n"]
+
+      body.each do |pile|
+        card = pile.first
+
+        print_with_color[:white, ' ']
+        print_with_color[:yellow, card[:cost]]
+        print_with_color[:red,    type_char[card[:type]]]
+        print_with_color[:blue,   '%-2i' % pile.size]
+        print_with_color[:white,  " %-#{max_name_length}s " % card[:name]]
+
+        print_with_color[:cyan_back,    card[:cards] || ' ']
+        print_with_color[:green_back,   card[:actions] || ' ']
+        print_with_color[:magenta_back, card[:buys] || ' ']
+        print_with_color[:yellow_back,  card[:gold] || ' ']
+
+        print_with_color[:white,  " %-#{max_name_length}s\n" % card[:description]]
+      end
+      wattr_set board_frame, A_NORMAL, 7, nil
+      box(board_frame, 0, 0)
+      board
+    end
+  end
 end
 
-Game.new.test
+Game.new.run
