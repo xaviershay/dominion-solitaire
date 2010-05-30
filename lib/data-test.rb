2bf16bd8993d98b27bfc1e27a8e7ddc359e7518f
Xavier Shay <xavier@rhnh.net>
2010-05-30 22:23:21 +1000

    autocomplete

--- lib/data-test.rb
+++ lib/data-test.rb
@@ -119,6 +119,7 @@ class Game
     ]
 
     @player = {
+      :current_input => '',
       :actions => 1,
       :hand    => [],
       :discard => [],
@@ -132,6 +133,8 @@ class Game
     #     [cards[:estate]] * 3
     #   )
     }
+
+    5.times { draw_card(@player) }
   end
 
   def test
@@ -147,8 +150,33 @@ class Game
   def run
     painter = Painter.new
     begin
-      ctx = painter.draw(self)
-      painter.wait_for_input(ctx)
+      running = true
+      while running == true
+
+
+        player[:prompt] = {
+          :prompt => '?',
+          :autocomplete => lambda {|input|
+            suggest = input.length == 0 ? nil : player[:hand].detect {|x|
+              x[:name] =~ /^#{input}/i
+            }
+            suggest ? suggest[:name] : nil
+          },
+          :accept => lambda {|input|
+            running = false
+          }
+        }
+        ctx = painter.draw(self)
+        ch = painter.wait_for_input(ctx)
+        case ch
+        when 10
+          player[:prompt][:accept][player[:current_input]]
+        when 127
+          player[:current_input] = player[:current_input][0..-2]
+        else
+          player[:current_input] += ch.chr
+        end
+      end
     ensure
       painter.finalize
     end
@@ -160,6 +188,7 @@ class Game
     def initialize
       initscr
       start_color
+      noecho
   
       # set up colour pairs
       #             Background       Foreground
@@ -193,76 +222,156 @@ class Game
     def draw(game)
       curs_set 0
      
-      board_frame = newwin(14, 80, 0, 0)
-      board = newwin(12, 78, 1, 1)
+      drawn = [{
+        :coords => [14, 80, 0, 0],
+        :title  => 'Board',
+        :draw   => lambda do |window, game|
+          print_with_color = lambda do |color, text|
+            color_index = {
+              :white        => 7,
+              :yellow       => 3,
+              :blue         => 4,
+              :red          => 1,
+              :cyan_back    => 14,
+              :green_back   => 10,
+              :magenta_back => 13,
+              :yellow_back  => 11
+            }[color] || raise("Unknown color: #{color}")
+            wattr_set window, A_NORMAL, color_index, nil
 
-      print_with_color = lambda do |color, text|
-        color_index = {
-          :white        => 7,
-          :yellow       => 3,
-          :blue         => 4,
-          :red          => 1,
-          :cyan_back    => 14,
-          :green_back   => 10,
-          :magenta_back => 13,
-          :yellow_back  => 11
-        }[color] || raise("Unknown color: #{color}")
-        wattr_set board, A_NORMAL, color_index, nil
+            wprintw(window, text.to_s)
+          end
 
-        wprintw(board, text.to_s)
+          type_char = {
+            :treasure => 'T',
+            :action   => 'A',
+            :victory  => 'V'
+          }
+
+          max_name_length = game.board.map {|pile| pile[0][:name].length }.max
+          
+          header, body = game.board.partition {|x|
+            [
+              :copper,
+              :silver,
+              :gold,
+              :estate,
+              :duchy,
+              :provence,
+              :curse
+            ].include?(x.first[:key])
+          }
+
+          header.each_with_index do |pile, i|
+            card = pile.first
+
+            print_with_color[:white, ' '] if i > 0
+            print_with_color[:yellow, card[:cost]]
+            print_with_color[:red,    type_char[card[:type]]]
+            print_with_color[:blue,   pile.size]
+            print_with_color[:white,  " %s" % card[:name]]
+          end
+
+          print_with_color[:white, "\n"]
+
+          body.each do |pile|
+            card = pile.first
+
+            print_with_color[:white, ' ']
+            print_with_color[:yellow, card[:cost]]
+            print_with_color[:red,    type_char[card[:type]]]
+            print_with_color[:blue,   '%-2i' % pile.size]
+            print_with_color[:white,  " %-#{max_name_length}s " % card[:name]]
+
+            print_with_color[:cyan_back,    card[:cards] || ' ']
+            print_with_color[:green_back,   card[:actions] || ' ']
+            print_with_color[:magenta_back, card[:buys] || ' ']
+            print_with_color[:yellow_back,  card[:gold] || ' ']
+
+            print_with_color[:white,  " %-#{max_name_length}s\n" % card[:description]]
+          end
+        end
+      }, {
+        :title => 'Your Turn',
+        :coords => [10, 80, 14, 0],
+        :draw => lambda do |window, game|
+          print = lambda do |text|
+            wprintw(window, text.to_s)
+          end
+          print["Hand: %s\n" % game.player[:hand].map {|x| x[:name] }.join(", ")]
+
+        end
+      }, {
+        :coords => [1, 80, 24, 0],
+        :border => false,
+        :draw => lambda do |window, game|
+          print_with_color = lambda do |color, text|
+            color_index = {
+              :white        => 7,
+              :yellow       => 3,
+              :blue         => 4,
+              :red          => 1,
+              :cyan_back    => 14,
+              :green_back   => 10,
+              :magenta_back => 13,
+              :yellow_back  => 11
+            }[color] || raise("Unknown color: #{color}")
+            wattr_set window, A_NORMAL, color_index, nil
+
+            wprintw(window, text.to_s)
+          end
+
+          if game.player[:prompt]
+            #print_with_color[:yellow_back, "%-80s" % ""]
+
+            input = game.player[:current_input]
+            suggest = game.player[:prompt][:autocomplete][input].to_s
+
+            print_with_color[:yellow_back, "%s %s" % [
+              game.player[:prompt][:prompt],
+              input]]
+
+            fill = suggest[input.length..-1]
+
+            if fill && fill.length > 0
+              print_with_color[:red, "%s" % fill]
+            end
+          else
+            print_with_color[:green_back, "%-80s" % ""]
+          end
+        end
+      }].map do |window|
+        window[:border] = true unless window.has_key?(:border)
+
+        c = window[:coords]
+        board_frame = newwin(*c)
+
+        if window[:border]
+          board = newwin(c[0] - 2, c[1] - 2, c[2] + 1, c[3] + 1)
+
+          window[:draw][board, game]
+
+          wattr_set board_frame, A_NORMAL, 7, nil
+          box(board_frame, c[2], c[3])
+          wmove(board_frame, 0, 2)
+          waddstr(board_frame, "| #{window[:title]} |")
+          wrefresh(board_frame)
+          wrefresh(board)
+          {
+            :frame => board_frame,
+            :inner => board
+          }
+        else
+          window[:draw][board_frame, game]
+          wrefresh(board_frame)
+          {
+            :frame => nil,
+            :inner => board_frame
+          }
+        end
       end
 
-      type_char = {
-        :treasure => 'T',
-        :action   => 'A',
-        :victory  => 'V'
-      }
-
-      max_name_length = game.board.map {|pile| pile[0][:name].length }.max
-      
-      header, body = game.board.partition {|x|
-        [
-          :copper,
-          :silver,
-          :gold,
-          :estate,
-          :duchy,
-          :provence,
-          :curse
-        ].include?(x.first[:key])
-      }
-
-      header.each do |pile|
-        card = pile.first
-
-        print_with_color[:yellow, card[:cost]]
-        print_with_color[:red,    type_char[card[:type]]]
-        print_with_color[:blue,   pile.size]
-        print_with_color[:white,  " %s" % card[:name]]
-        print_with_color[:white, ' ']
-      end
-
-      print_with_color[:white, "\n"]
-
-      body.each do |pile|
-        card = pile.first
-
-        print_with_color[:white, ' ']
-        print_with_color[:yellow, card[:cost]]
-        print_with_color[:red,    type_char[card[:type]]]
-        print_with_color[:blue,   '%-2i' % pile.size]
-        print_with_color[:white,  " %-#{max_name_length}s " % card[:name]]
-
-        print_with_color[:cyan_back,    card[:cards] || ' ']
-        print_with_color[:green_back,   card[:actions] || ' ']
-        print_with_color[:magenta_back, card[:buys] || ' ']
-        print_with_color[:yellow_back,  card[:gold] || ' ']
-
-        print_with_color[:white,  " %-#{max_name_length}s\n" % card[:description]]
-      end
-      wattr_set board_frame, A_NORMAL, 7, nil
-      box(board_frame, 0, 0)
-      board
+      drawn[0][:inner]
     end
   end
 end
