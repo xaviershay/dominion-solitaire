eddcfe6fe52e764893ad27e1365b213d0608f673
Xavier Shay <xavier@rhnh.net>
2010-06-14 10:55:29 +1000

    Fix spy specs

--- spec/cards/spy_spec.rb
+++ spec/cards/spy_spec.rb
@@ -6,23 +6,25 @@ describe_card :spy do
 
   describe 'when played' do
     it 'reveals top card and allows to discard' do
-      deck cards(:estate, 1)
+      deck cards(:copper, 1) + cards(:estate, 1)
 
       playing_card do
         input 'Y'
       end
 
+      hand.should    have_cards(cards(:copper, 1))
       deck.should    have_cards([])
       discard.should have_cards(cards(:estate, 1))
     end
 
     it 'reveals top card and allows to put back' do
-      deck cards(:estate, 1)
+      deck cards(:copper, 1) + cards(:estate, 1)
 
       playing_card do
         input 'N'
       end
 
+      hand.should    have_cards(cards(:copper, 1))
       discard.should have_cards([])
       deck.should    have_cards(cards(:estate, 1))
     end
